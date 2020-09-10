7c40e8857b27ff64fe4513a4b44563e8d0734da7
yu.chenyang <yu.chenyang@qianka.com>
2020-09-10 14:32:46 +0800

    Add: init react

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './theme/theme.less';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
