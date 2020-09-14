e2c8f152a9155e8ffb896f912a5a0f0aa8605bea
yu.chenyang <yu.chenyang@qianka.com>
2020-09-14 18:57:35 +0800

    Add: 钱包设置

--- client/src/components/AccountCard.js
+++ client/src/components/AccountCard.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardHeader, CardContent, CardActions, Box, IconButton } from "@material-ui/core";
+import Typography from '@material-ui/core/Typography';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Chip from '@material-ui/core/Chip';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const AccountCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+const useStyle = makeStyles((theme) => ({
+  balance: {
+    textAlign: "center"
+  },
+  action: {
+    marginLeft: "auto"
+  }
+}));
+
+export default function AccountCard(props) {
+  const classes = useStyle();
+  const web3 = React.$web3;
+
+  let splitAddress = (address) => {
+    return `${address.substring(0,4)}...${address.substr(-7)}`
+  }
+
+  return (
+    <Card>
+      <CardHeader
+        title={
+          <Typography cariant="h5">
+            我的钱包
+          </Typography>
+        }
+      >
+      </CardHeader>
+      <CardContent>
+        <Box className={classes.balance}>
+          <img src="/ETH.svg" style={{height: "3rem", width:"3rem"}}></img>
+            <Typography variant="h4">
+              {web3.utils.fromWei(String(props.balance), "ether") }
+            </Typography>
+            <Tooltip title={props.address} placement="bottom" disableFocusListener style={{marginTop: "1vh"}}>
+              <Chip color="primary" size="small" label={splitAddress(props.address)} icon={<AccountCircleIcon/>} />
+            </Tooltip>
+        </Box>
+      </CardContent>
+      <CardActions>
+        <IconButton className={classes.action} aria-label="refresh" onClick={ async () => {
+          await props.onRefresh();
+        } } color="primary">
+          <RefreshIcon/>
+        </IconButton>
+        <IconButton className={classes.action} aria-label="delete" onClick={props.onDelete} color="primary">
+          <DeleteIcon/>
+        </IconButton>
+      </CardActions>
+
+    </Card>
+  );
 };
-
-export default AccountCard;
--- client/src/pages/Wallet.js
+++ client/src/pages/Wallet.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AccountStore from '../store/data/account';
 import { makeStyles } from "@material-ui/core/styles";
 import Card from '@material-ui/core/Card';
@@ -6,7 +6,16 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import account from "../store/data/account";
+import Box from '@material-ui/core/Box'
+import AccountCard from "../components/AccountCard";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import SendIcon from '@material-ui/icons/Send';
+import LocalGasStationIcon from '@material-ui/icons/LocalGasStation';
+import LinkIcon from '@material-ui/icons/Link';
+import Paper from "@material-ui/core/Paper";
 
 
 const useStyle = makeStyles((theme) => ({
@@ -16,6 +25,14 @@ const useStyle = makeStyles((theme) => ({
     justifyContent: "center",
     height: "100%"
   },
+  rootUnlock: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%"
+  },
+  accountCard: {
+    paddingTop: "2vh"
+  },
   unlockContent: {
     display: "flex",
     flexDirection: "column",
@@ -23,6 +40,9 @@ const useStyle = makeStyles((theme) => ({
   },
   title: {
     fontSize: 14
+  },
+  settingList: {
+    marginTop: "1vh"
   }
 }));
 
@@ -30,6 +50,23 @@ const useStyle = makeStyles((theme) => ({
 export default function Wallet(props) {
   const classes = useStyle();
   const { state: accountState ,dispatch: AccountDispatch } = React.useContext(AccountStore.Context);
+  const web3 = React.$web3;
+
+  useEffect(()=> {
+    let fetch_balance = async () => {
+      if (accountState.unLock) {
+        let balance = await web3.eth.getBalance(accountState.address);
+        console.log(balance);
+        AccountDispatch({
+          type: "Balance",
+          payload: {
+            balance: balance
+          }
+        })
+      }
+    }
+    fetch_balance();
+  }, []);
 
   function unLockDialog() {
     return (
@@ -52,16 +89,59 @@ export default function Wallet(props) {
     )
 }
 
-  function getWallet(params) {
+  function getWallet() {
     if (accountState.unLock) {
-      return <p>Wallet</p>
+      return (
+        <Box className={classes.accountCard}>
+          <AccountCard
+            balance={accountState.balance}
+            address={accountState.address}
+            onRefresh={ async ()=> {
+              let balance = await web3.eth.getBalance(accountState.address);
+              AccountDispatch({
+                type: "Balance",
+                payload: {
+                  balance: String(balance)
+                }
+              });
+            }}
+            onDelete={()=> {
+              window.localStorage.removeItem("keystore");
+              props.history.push("/Greeting");
+            }}
+          >
+          </AccountCard>
+          <Paper className={classes.settingList}>
+            <List>
+              <ListItem button>
+                <ListItemIcon>
+                  <SendIcon />
+                </ListItemIcon>
+                <ListItemText primary="转账" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <LocalGasStationIcon />
+                </ListItemIcon>
+                <ListItemText primary="查看Gas价格" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <LinkIcon />
+                </ListItemIcon>
+                <ListItemText primary="设置以太坊网络" />
+              </ListItem>
+            </List>
+          </Paper>
+        </Box>
+      )
     } else {
       return unLockDialog();
     }
     
   }
   return (
-    <div className={classes.root}>
+    <div className={accountState.unLock?classes.rootUnlock:classes.root}>
       {getWallet()}
     </div>
   );
